b4719cc85973805cb35e7edbe736ad1e8907f923
DoDoGaMaRu <nm09090@gmail.com>
2022-07-27 00:39:55 +0900

    temp commit

--- createModel/ModelCreator.py
+++ createModel/ModelCreator.py
@@ -19,7 +19,9 @@ del tokenized_data
 
 from gensim.models import Word2Vec
 print('make model...')
-model = Word2Vec(sentences = train_data, vector_size = 100, window = 5, min_count = 5, workers = 4, sg = 0)
+model = Word2Vec(sentences = train_data, vector_size = 300, window = 5, min_count = 5, workers = 4, sg = 0)
+
+print(model.wv.vectors.shape)
 
 print('save model...')
 model.wv.save_word2vec_format('../data/model/music_w2v')
