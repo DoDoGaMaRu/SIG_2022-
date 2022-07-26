6178865562a6f0611fbd5890e8837716db69df5c
DoDoGaMaRu <nm09090@gmail.com>
2022-07-26 22:40:46 +0900

    temp commit

--- createToken/TokenCreator.py
+++ createToken/TokenCreator.py
@@ -9,18 +9,21 @@ def createTokenJson(df, fileName) :
     okt = Okt()
 
     tokenized_data = []
-    for lyrics in tqdm(df['lyrics']) :
-        for sentence in lyrics :
+    for lyrics in tqdm(df['lyrics']):
+        tokenized_music = []
+        for sentence in lyrics:
             tokenized_sentence = okt.morphs(sentence, stem=True) # 토큰화
-
-            if tokenized_sentence:
-                tokenized_data.append(tokenized_sentence)
+            tokenized_music.append(tokenized_sentence)
 
             del tokenized_sentence
+
+        tokenized_data.append(tokenized_music)
+
 
     with open("../data/token//" + fileName + ".json", 'w', encoding="utf-8") as f:
         json.dump(tokenized_data, f, ensure_ascii=False)
 
+    del df
     del tokenized_data
 
 
@@ -29,6 +32,7 @@ def getDataFrame(path) :
     print("\topen dataFrame...")
     with open(path, 'r') as f:
         data = json.loads(f.read())
+        del f
     print("\topen complete")
 
     print("\tmake table...")
@@ -42,6 +46,7 @@ def getDataFrame(path) :
 def run(count, thread, size) :
     df = getDataFrame("../data/dataFrame//musicDataFrame.json")
 
+    print("\trun processes...")
     processes = []
     for idx in range(thread * count, thread * (count + 1)) :
         start = size * idx
@@ -60,30 +65,38 @@ def run(count, thread, size) :
 def makeFile(thread, loop) :
     token_full = []
 
-    for idx in range(0, thread * loop) :
+    print("\tmerge token files...")
+    for idx in tqdm(range(0, thread * loop + 1)) :
         with open("../data/token/" + str(idx) + ".json", 'r', encoding="utf-8") as f:
             data = json.loads(f.read())
 
             token_full += data
             del data
+            del f
 
-    with open("../data/token//token_full.json", 'w', encoding="utf-8") as f:
+    print("\tsave tokenized_data...")
+    with open("../data/token//tokenized_data.json", 'w', encoding="utf-8") as f:
         json.dump(token_full, f, ensure_ascii=False)
 
 
 # main
 if __name__ == '__main__':
-    thread = 8
-    loop = 4
+    thread = 16
+    loop = 14
 
     print("[check size...]")
     df = getDataFrame("../data/dataFrame//musicDataFrame.json")
-    size = len(df) // thread // loop
+    full_size = len(df)
+    size = full_size // thread // loop
+    print("\tfull_size = " + str(full_size))
+
+    createTokenJson(df.iloc[thread * loop * size:], str(thread * loop))
     del df
 
-    for count in range(0,loop) :
+    for count in range(0, loop) :
         print("[Run " + str(count) + "]")
         run(count, thread, size)
+
 
     print("[make file...]")
     makeFile(thread, loop)
